0ff205677d90568414043858e4503fef1c16c921
Lszk <WH__@hotmail.com>
2015-12-17 18:30:20 -0600

    1217

--- Platformer.java
+++ Platformer.java
@@ -15,7 +15,8 @@ public class Platformer {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        // TODO code application logic here
+        int x = 1;
+        int y = x*3;
     }
     
 }
